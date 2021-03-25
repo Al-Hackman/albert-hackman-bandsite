97c427b3ef15e0b9798232f0bf6437e578864c34
Albert Hackman <nana20p@yahoo.com>
2021-03-25 08:34:55 -0400

    Information/Data retieved and populated on Comments/Shows Pages

---
+++ sprint-three/scripts/build-shows.page.js
@@ -0,0 +1,139 @@
+
+//Declaring an Array of objects with the dafault details in mockup
+
+// let showsArray = [
+//     {
+//         Date:"Mon Dec 17 2018",
+//         Venue: "Ronald Lane",
+//         Location:"San Francisco, CA",
+//     },
+//     {
+//         Date:"Tue 18 2019",
+//         Venue: "Pier 3 East",
+//         Location:"San Francisco, CA",
+//     },
+//     {
+//         Date:"Fri Jul 22 2019",
+//         Venue: "View Loungue",
+//         Location:"San Francisco, CA",
+//     },
+//     {
+//         Date:"Sat Aug 12 2019",
+//         Venue: "Hyatt Agency",
+//         Location:"San Francisco, CA",
+//     },
+//     {
+//         Date:"Fri Sep 05 2019",
+//         Venue: "Moscow Center",
+//         Location:"San Francisco, CA",
+//     },
+//     {
+//         Date:"Wed Aug 11 2019",
+//         Venue: "Pres Club",
+//         Location:"San Francisco, CA",
+//     }
+// ];
+
+
+let bandsiteAPI = 'https://project-1-api.herokuapp.com/';
+let showsEndpoint = 'showdates';
+let postEndpoint = 'post/';
+let keyApi = `?api_key=bab75cb4-c532-4fcd-add1-0c2b0361be14`;
+
+axios.get(`${bandsiteAPI}${showsEndpoint}${keyApi}`)
+    .then(response => {
+        console.log(response.data);
+        let showsArray = response.data;
+        displayShows(showsArray);
+    })
+    .catch(error => {
+        console.log('Uh oh! There was an error:', error)
+    })
+
+//Declaring a function which will create different elements whenever this function is called.
+//Again this function takes the array created above as an argument to display the values. 
+
+displayShows = (arr) => {
+        let showsContainer = document.querySelector(".shows-container");
+       
+        let showsHeader = document.createElement("h2");
+        showsHeader.classList.add("shows__header");
+        showsHeader.innerText = "Shows";
+        showsContainer.appendChild(showsHeader);
+
+        let showsCover = document.createElement("div");
+        showsCover.classList.add("shows__cover");
+        showsContainer.appendChild(showsCover);
+
+        let showsTitles = document.createElement("div");
+        showsTitles.classList.add("shows__titles");
+        showsCover.appendChild(showsTitles);
+
+        let title_1 = document.createElement("h4");
+        title_1.classList.add("shows__title");
+        title_1.innerText = "DATES";
+        showsTitles.appendChild(title_1);
+
+        let title_2 = document.createElement("h4")
+        title_2.classList.add("shows__title");
+        title_2.innerText = "VENUE";
+        showsTitles.appendChild(title_2);
+
+        let title_3 = document.createElement("h4")
+        title_3.classList.add("shows__title");
+        title_3.innerText = "LOCATION";
+        showsTitles.appendChild(title_3);
+         
+
+    for (let i = 0; i < arr.length; i++){
+
+        let showsCont = document.createElement("div");
+
+        showsCont.classList.add("shows");
+        showsCover.appendChild(showsCont);
+        
+        let showsList = document.createElement("ul");
+        showsList.classList.add("shows__list");
+        showsCont.appendChild(showsList);
+
+        let showsItem = document.createElement("li");
+        showsItem.classList.add("shows__item");
+        showsItem.innerText = "DATE";
+        showsList.appendChild(showsItem);
+        //
+        let showDate = document.createElement("span");
+        showDate.classList.add("shows__item-date");
+        showDate.innerText =arr[i].date;
+        showsList.appendChild(showDate);
+
+        let showsItem2 = document.createElement("li");
+        showsItem2.classList.add("shows__item");
+        showsItem2.innerText = "VENUE";
+        showsList.appendChild(showsItem2);
+        //
+        let showVenue = document.createElement("span");
+        showVenue.classList.add("shows__item-venue");
+        showVenue.innerText = arr[i].place;
+        showsList.appendChild(showVenue);
+
+        let showsItem3 = document.createElement("li");
+        showsItem3.classList.add("shows__item");
+        showsItem3.innerText = "LOCATION";
+        showsList.appendChild(showsItem3);
+        //
+        let showLocation = document.createElement("span");
+        showLocation.classList.add("shows__item-location");
+        showLocation.innerText = arr[i].location;
+        showsList.appendChild(showLocation);
+
+        let showButton = document.createElement("button");
+        showButton.classList.add("shows__button");
+        showButton.innerText = "BUY TICKETS";
+        showsCont.appendChild(showButton);
+
+    }
+}
+
+//This is to Invoke(call) the function, with the array as it's argument.
+
+// displayShows(showsArray);
---
+++ sprint-three/scripts/index-page.js
@@ -0,0 +1,140 @@
+
+//Declaring an Array with the default 3 comments given in the mockup.
+
+// let commentArray = [
+//     {
+//         name:"Michael Lyons",
+//         date: "12/18/2018",
+//         statement:"They BLEW the ROOF off their last show, once everyone started figuring out they were going. This is still simply the greatest opening of concert I have EVER witnessed.",
+//     },
+//     {
+//         name: "Gary Wong",
+//         date: "12/12/2018",
+//         statement: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself.",
+//     },
+//     {
+//         name: "Theodore Duncan",
+//         date: "11/15/2018",
+//         statement: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He's definitely my favourite ever!"
+//     }
+// ];
+
+
+
+let bandsiteAPI = 'https://project-1-api.herokuapp.com/';
+let commentEndpoint = 'comments';
+let postEndpoint = 'post/';
+let keyApi = `?api_key=bab75cb4-c532-4fcd-add1-0c2b0361be14`;
+
+axios.get(`${bandsiteAPI}${commentEndpoint}${keyApi}`)
+    .then(response => {
+        console.log(response.data);
+        let commentArray = response.data;
+        duplicate(commentArray)
+    })
+    .catch(error => {
+        console.log('Uh oh! There was an error:', error)
+    })
+
+
+//Declaring a function to create various elements and add values as well based on the arguments.
+
+duplicate = (arr) => {
+
+    let commentContainer = document.querySelector(".saved-comment");
+    
+    for(let i = 0; i < arr.length; i++){
+
+        let photoComment = document.createElement("div");
+        photoComment.classList.add("saved-comment__cover");
+        commentContainer.appendChild(photoComment);
+
+        let photo = document.createElement("div");
+        photo.classList.add("saved-comment__photo");
+        photoComment.appendChild(photo);
+
+        let pastComment = document.createElement("div");
+        pastComment.classList.add("saved-comment__comments");
+        photoComment.appendChild(pastComment);
+
+        let commentName = document.createElement("h4");
+        commentName.classList.add("saved-comment__name");
+        commentName.innerText = arr[i].name;
+        pastComment.appendChild(commentName);
+
+         // Convert timestamp to date
+        let date = new Date(arr[i].timestamp);
+        let month = date.getMonth() +1;
+        let day = date.getDate();
+        let year = date.getFullYear();
+
+        let commentDate = document.createElement("span");
+        commentDate.classList.add("saved-comment__date");
+        commentDate.innerText = `${month}/${day}/${year}`,
+        commentName.appendChild(commentDate);
+
+        let mainComment = document.createElement("p");
+        mainComment.classList.add("saved-comment__content");
+        mainComment.innerText = arr[i].comment;
+        pastComment.appendChild(mainComment);
+    }
+}
+
+//Invoking(calling) the function with the array as the argument.
+
+// duplicate(commentArray);
+
+
+//Creating an Event Handler to add new comment.
+// This function also clears up the old history on saved comments and builds a new array with the latest comment on top.
+
+let submitButton = document.querySelector('#submit-comment');
+let textName = document.querySelector('#full-name');
+let textComment = document.querySelector('#input-comment');
+
+displayComment = event => {
+    event.preventDefault();
+
+    document.querySelector('.saved-comment').innerHTML = '';
+
+    let item = document.createElement("h4");
+    let textName = document.querySelector('#full-name').value;
+    item.innerText = textName;
+
+    let item2 = document.createElement("p");
+    let textComment = document.querySelector("#input-comment").value;
+    item2.innerText = textComment;
+
+    let today = new Date();
+    let month = today.getMonth() +1;
+    let year = today.getFullYear();
+    let date = today.getDate();
+    
+    let newCom = {
+        name: textName,
+        timestamp: `${month}/${date}/${year}`,
+        comment: textComment,
+    }
+
+ 
+    axios.post(bandsiteAPI+postEndpoint+commentEndpoint+keyApi, {name:textName,timestamp: `${month}/${date}/${year}`,comment: textComment,})
+    .then(response => {
+        console.log(response)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+    // commentArray.unshift(newCom);
+    // commentArray.pop();
+   
+    // duplicate(commentArray);
+
+    
+    document.querySelector('#full-name').value = ' ';
+    document.querySelector('#input-comment').value = ' ';
+}
+
+//Invoke(call) the function when the button is clicked.
+
+submitButton.addEventListener('click', displayComment);

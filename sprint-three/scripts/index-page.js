1c7801581c2cd9e8b522411999a5caf4b25b49ba
Albert Hackman <nana20p@yahoo.com>
2021-03-26 10:58:11 -0400

    Worked on pushing comments to the API and to the comment section

--- sprint-three/scripts/index-page.js
+++ sprint-three/scripts/index-page.js
@@ -25,17 +25,19 @@ let bandsiteAPI = 'https://project-1-api.herokuapp.com/';
 let commentEndpoint = 'comments';
 let postEndpoint = 'post/';
 let keyApi = `?api_key=bab75cb4-c532-4fcd-add1-0c2b0361be14`;
-
-axios.get(`${bandsiteAPI}${commentEndpoint}${keyApi}`)
+getAllComments = () =>{
+    axios.get(`${bandsiteAPI}${commentEndpoint}${keyApi}`)
     .then(response => {
         console.log(response.data);
         let commentArray = response.data;
-        duplicate(commentArray)
+        duplicate(commentArray.reverse().slice(0, 3))
     })
     .catch(error => {
         console.log('Uh oh! There was an error:', error)
     })
+}
 
+getAllComments();
 
 //Declaring a function to create various elements and add values as well based on the arguments.
 
@@ -43,8 +45,8 @@ duplicate = (arr) => {
 
     let commentContainer = document.querySelector(".saved-comment");
     
-    for(let i = 0; i < arr.length; i++){
-
+    arr.forEach(element => {
+        
         let photoComment = document.createElement("div");
         photoComment.classList.add("saved-comment__cover");
         commentContainer.appendChild(photoComment);
@@ -59,11 +61,11 @@ duplicate = (arr) => {
 
         let commentName = document.createElement("h4");
         commentName.classList.add("saved-comment__name");
-        commentName.innerText = arr[i].name;
+        commentName.innerText = element.name;
         pastComment.appendChild(commentName);
 
          // Convert timestamp to date
-        let date = new Date(arr[i].timestamp);
+        let date = new Date(element.timestamp);
         let month = date.getMonth() +1;
         let day = date.getDate();
         let year = date.getFullYear();
@@ -75,9 +77,33 @@ duplicate = (arr) => {
 
         let mainComment = document.createElement("p");
         mainComment.classList.add("saved-comment__content");
-        mainComment.innerText = arr[i].comment;
+        mainComment.innerText = element.comment;
         pastComment.appendChild(mainComment);
-    }
+
+        let likeDelete = document.createElement("div");
+        likeDelete.classList.add("saved-comment__like-del");
+        pastComment.appendChild(likeDelete);
+
+        let likeCount = document.createElement("span");
+        likeCount.classList.add("saved-comment__count");
+        likeDelete.appendChild(likeCount).innerHTML=element.likes;
+
+        let likeComment = document.createElement("img");
+        likeComment.classList.add("saved-comment__ld");
+        likeComment.setAttribute("src","./assets/icons/PNG/like.png");
+        likeComment.setAttribute('alt', "Like this comment");
+        likeComment.setAttribute('data-comment_id', element.id);
+        likeComment.setAttribute('data-like_count', element.likes);
+        likeComment.addEventListener('click', likeThisComment)
+        likeDelete.appendChild(likeComment);
+
+        let delComment = document.createElement("img");
+        delComment.classList.add("saved-comment__ld");
+        delComment.setAttribute("src","./assets/icons/PNG/delete.png");
+        delComment.setAttribute('alt', "Delete Comment");
+        likeDelete.appendChild(delComment);
+    
+});
 }
 
 //Invoking(calling) the function with the array as the argument.
@@ -111,15 +137,15 @@ displayComment = event => {
     let date = today.getDate();
     
     let newCom = {
-        name: textName,
-        timestamp: `${month}/${date}/${year}`,
-        comment: textComment,
+	"name": textName,
+	"comment": textComment,
     }
 
  
-    axios.post(bandsiteAPI+postEndpoint+commentEndpoint+keyApi, {name:textName,timestamp: `${month}/${date}/${year}`,comment: textComment,})
+    axios.post(`${bandsiteAPI}${commentEndpoint}${keyApi}`, newCom, {headers: {'Content-Type': 'application/json'}} )
     .then(response => {
-        console.log(response)
+        console.log(response);
+        getAllComments();
     })
     .catch(err => {
         console.log(err)
@@ -131,10 +157,18 @@ displayComment = event => {
     // duplicate(commentArray);
 
     
-    document.querySelector('#full-name').value = ' ';
-    document.querySelector('#input-comment').value = ' ';
+    document.querySelector('#full-name').value = '';
+    document.querySelector('#input-comment').value = '';
 }
+
+// function likeThisComment() {
+//     console.log('this comment', this.dataset.comment_id)
+//     console.log('this comment count', this.dataset.like_count++)
+//     // call api to update the likes
+// }
 
 //Invoke(call) the function when the button is clicked.
 
 submitButton.addEventListener('click', displayComment);
+
+// delComment.addEventListener('click', deleteComment(id))
